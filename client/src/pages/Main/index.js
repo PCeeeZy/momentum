47de5600968cfba0ebf27c44d3782f31142c4edc
PCwik88 <paul@cwik.us>
2020-02-05 20:44:03 -0600

    fixing main folder

---
+++ client/src/pages/Main/index.js
@@ -0,0 +1,24 @@
+import React, { Component} from "./node_modules/react";
+
+import Time from "../../components/Time";
+
+class Main extends Component {
+    state = {
+
+    };
+
+    componentDidMount() {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <Time />
+                Main says hello world
+            </div>
+        )
+    }
+}
+
+export default Main;
